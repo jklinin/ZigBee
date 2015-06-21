dc4feb6ec339cf2280e111356ba4792f552e2231
jklinin <yuri.kalinin@outlook.de>
2015-06-21 15:19:06 +0200

    added method for converting icon to image

--- src/org/project/mw/util/Util.java
+++ src/org/project/mw/util/Util.java
@@ -2,6 +2,9 @@ package org.project.mw.util;
 
 import java.awt.Component;
 import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
@@ -13,13 +16,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 
 import org.project.mw.gui.Element;
-
 
 /**
  * @author yuri
@@ -32,7 +35,6 @@ public class Util {
 																// elements(element
 																// name, x, y
 																// position)
-	
 
 	public static Util getInstance() {
 		if (utilInstance == null) {
@@ -137,19 +139,22 @@ public class Util {
 	}
 
 	public void removeAllElementsArray() {
-		elementsArray=new ArrayList<Element>();
+		elementsArray = new ArrayList<Element>();
 
 	}
 
-
 	/**
-	 * The method make scalation of Image 
-	 * @param srcImg source image 
-	 * @param w width
-	 * @param h height
+	 * The method make scalation of Image
+	 * 
+	 * @param srcImg
+	 *            source image
+	 * @param w
+	 *            width
+	 * @param h
+	 *            height
 	 * @return resized image icon
 	 */
-	public ImageIcon getScaledImage(String imageName, int w, int h) {		
+	public ImageIcon getScaledImage(String imageName, int w, int h) {
 		Toolkit t = Toolkit.getDefaultToolkit();
 		Image srcImg = t.getImage(imageName);
 		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
@@ -157,15 +162,33 @@ public class Util {
 		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 		g2.drawImage(srcImg, 0, 0, w, h, null);
 		g2.dispose();
-		return  new ImageIcon(resizedImg);
+		return new ImageIcon(resizedImg);
 	}
-	public Image getScaledImage(Image srcImg, int w, int h) {		
+
+	public Image getScaledImage(Image srcImg, int w, int h) {
 
 		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = resizedImg.createGraphics();
 		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 		g2.drawImage(srcImg, 0, 0, w, h, null);
 		g2.dispose();
-		return  resizedImg;
+		return resizedImg;
+	}
+
+	public Image iconToImage(Icon icon) {
+		if (icon instanceof ImageIcon) {
+			return ((ImageIcon) icon).getImage();
+		} else {
+			int w = icon.getIconWidth();
+			int h = icon.getIconHeight();
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			GraphicsDevice gd = ge.getDefaultScreenDevice();
+			GraphicsConfiguration gc = gd.getDefaultConfiguration();
+			BufferedImage image = gc.createCompatibleImage(w, h);
+			Graphics2D g = image.createGraphics();
+			icon.paintIcon(null, g, 0, 0);
+			g.dispose();
+			return image;
+		}
 	}
 }
