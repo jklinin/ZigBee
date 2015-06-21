8f7e48eadd19676daa695a506d8fba8fad200e22
jklinin <yuri.kalinin@outlook.de>
2015-06-21 08:56:01 +0200

    add remove funktion temp

--- src/org/project/mw/gui/ClickListner.java
+++ src/org/project/mw/gui/ClickListner.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
+import org.project.mw.util.Util;
+
 public class ClickListner implements ActionListener {
 
 	@Override
@@ -13,7 +15,29 @@ public class ClickListner implements ActionListener {
 		JButton component = (JButton) e.getSource();
 		CellPane parentComp = (CellPane) component.getParent();
 		System.out.println("Parent index " + parentComp.getIndex());
+removeElement( parentComp.getIndex());
+	}
+	
+	public void removeElement(int indexComp){
+	
+			
+			for (int i = 0; i < paneModelCentre.getLabelArray().size(); i++) {
+				if (paneModelCentre.getLabelArray().get(i).getIcon() != null) {
+					icon = paneModelCentre.getLabelArray().get(i).getIcon().toString();
+					paneModelCentre.getLabelArray().get(i).setIcon(Util.getInstance().getScaledImage(icon, modelDemension, modelDemension));
 
+					if (icon.contains("@") == false) {
+						Util.getInstance().getElementsArray().get(i).setFileIconName(icon);
+						paneModelCentre.getLabelArray().get(i).setIcon(Util.getInstance().getScaledImage(icon, modelDemension, modelDemension));
+					}
+				}
+			}
+			paneModelCentre.update(scalFactor, modelDemension, modelDemension, -1);// FIXME
+			
+
+		
+	
+		
 	}
 
 }
