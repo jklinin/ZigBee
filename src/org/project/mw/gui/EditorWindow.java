b9c7e8680e9bec03a22dcfa9fcdc624a0ec44572
jklinin <yuri.kalinin@outlook.de>
2015-06-23 20:21:55 +0200

    added disponse to onclose

--- src/org/project/mw/gui/EditorWindow.java
+++ src/org/project/mw/gui/EditorWindow.java
@@ -71,6 +71,50 @@ public class EditorWindow extends JFrame {
 		setIconImage(((ImageIcon) UIManager.getIcon("FileView.computerIcon")).getImage());
 		KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
 		manager.addKeyEventDispatcher(new MyDispatcher());
+		addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				editWindowInstanze.dispose();
+				
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		setMinimumSize(new Dimension(1133, 652));
 		Container editWindowContentPane = getContentPane();
 		editWindowContentPane.setLayout(new BorderLayout());
