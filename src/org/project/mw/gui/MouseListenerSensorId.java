02a446403252f751f0ccd854fc889533a56c2f44
jklinin <yuri.kalinin@outlook.de>
2015-07-02 10:40:57 +0200

    added regular exp(only int)

--- src/org/project/mw/gui/MouseListenerSensorId.java
+++ src/org/project/mw/gui/MouseListenerSensorId.java
@@ -1,6 +1,11 @@
 package org.project.mw.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Point;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -11,6 +16,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import org.project.mw.util.Util;
 
@@ -29,21 +36,66 @@ public class MouseListenerSensorId extends MouseAdapter {
 			if (Util.getInstance().getElementsCollection().get(new Point(x, y)).isSensor() == true) {
 				System.out.println("Sensor");
 
-				JTextField sensorIDField = new JTextField(5);
+				dialog(x, y);
 
-				JPanel myPanel = new JPanel();
-				myPanel.add(new JLabel("Sensor ID"));
-				myPanel.add(sensorIDField);
-				myPanel.add(Box.createHorizontalStrut(15));
-
-				int result = JOptionPane.showConfirmDialog(null, myPanel, "Sensor ID", JOptionPane.OK_CANCEL_OPTION);
-				if (result == JOptionPane.OK_OPTION) {
-					System.out.println("id value: " + sensorIDField.getText());
-					int id = Integer.parseInt(sensorIDField.getText());
-					Util.getInstance().getElementsCollection().get(new Point(x, y)).setSensorID(id);
-
-				}
 			}
 		}
+	}
+
+	private void dialog(int x, int y) {
+		JPanel pan = new JPanel(new BorderLayout());
+		final JTextField sensorIdField = new JTextField(5);
+		final JButton ok = new JButton("OK");
+		ok.setEnabled(false);
+
+		ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String input = sensorIdField.getText();
+				System.out.println("The input is: " + input);
+				int id = Integer.parseInt(input);
+				Util.getInstance().getElementsCollection().get(new Point(x, y)).setSensorID(id);
+
+				/* close the dialog */
+				Window w = SwingUtilities.getWindowAncestor(ok);
+				if (w != null)
+					w.setVisible(false);
+			}
+		});
+
+		sensorIdField.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				if (sensorIdField.getText().matches("\\d+") == false) {
+					ok.setEnabled(false);
+				} else {
+					ok.setEnabled(true);
+				}
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+
+				if (sensorIdField.getText().matches("\\d+") == true) {
+					ok.setEnabled(true);
+				} else {
+					ok.setEnabled(false);
+				}
+
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				if (sensorIdField.getText().matches("\\d+") == true) {
+					ok.setEnabled(true);
+				} else {
+					ok.setEnabled(false);
+				}
+			}
+		});
+
+		pan.add(sensorIdField, BorderLayout.NORTH);
+		JOptionPane.showOptionDialog(null, pan, "Sensor ID", JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, new JButton[] { ok }, ok);
+
 	}
 }
